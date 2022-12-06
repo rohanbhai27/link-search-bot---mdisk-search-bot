f4882eac082ae5fde5e729d31c83aac8b6772c8c
rohanbhai27 <113032419+rohanbhai27@users.noreply.github.com>
2022-12-06 15:26:58 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-echo "Cloning Repo...."
-git clone https://github.com/Greymattersbot/link-search-bot---mdisk-search-bot /Mdisk-Search-Bot
-cd /Mdisk-Search-Bot
-pip3 install -r requirements.txt
-echo "Starting Bot....GreyMatter_Bots"
-python3 main.py
