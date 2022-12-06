5e90ccc419c8de12b39be0ccf81a50be1d94281c
rohanbhai27 <113032419+rohanbhai27@users.noreply.github.com>
2022-12-06 15:48:21 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/rohanbhai27/link-search-bot---mdisk-search-bot /Mdisk-Search-Bot
+cd /Mdisk-Search-Bot
+pip3 install -r requirements.txt
+echo "Starting Bot....Rnp_Developer"
+python3 main.py
